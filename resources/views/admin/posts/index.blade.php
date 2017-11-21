0ce632224b64247f2deee11a9606b1337230b9b9
LeoHuang <leohuang1996@gmail.com>
2017-11-22 02:00:45 +0800

    '練習三:修正錯誤'

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -40,11 +40,11 @@
                 <tbody>
                 @foreach($posts as $post)
                     <tr>
-                        <td>{{$post‐>id}}</td>
-                        <td>{{$post‐>title}}</td>
-                        <td>{{($post‐>is_feature)?'v':'x'}}</td>
+                        <td>{{ $post -> id }}</td>
+                        <td>{{ $post -> title }}</td>
+                        <td>{{ ($post -> is_feature)? 'v' : 'x' }}</td>
                         <td>
-                            <a href="{{ route('admin.posts.edit', $post->$id) }}">編輯</a>
+                            <a href="{{ route('admin.posts.edit',  $post -> id) }}">編輯</a>
                             /
                             <a href="#">刪除</a>
                         </td>
