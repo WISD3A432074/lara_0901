daa9eb5da263d5f2782755d6fc05b4b2810b0d7d
LeoHuang <leohuang1996@gmail.com>
2017-11-28 22:45:54 +0800

    '練習七:刪除資料'

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -40,4 +40,9 @@ class AdminPostsController extends Controller
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
  }
+     public function destroy($id)
+     {
+     Post::destroy($id);
+     return redirect()->route('admin.posts.index');
+     }
 }
